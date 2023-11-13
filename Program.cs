615312629c47e320a2d3a3739f26861aaf6537ae
Diego Ruiz <ruizrecharted@gmail.com>
2023-11-13 16:15:36 -0500

    despide diego:commit 6

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
 Console.WriteLine("Hello, World!");
 //Diego ola
 //ola como esats
@@ -6,3 +8,5 @@ Console.WriteLine("Hello, World!");
 //ola
 //DIEGO 3 
 //DIEGO 4
+Diego diego = new Diego();
+diego.Despide();
